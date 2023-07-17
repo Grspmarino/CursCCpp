9f315f81647eed626fa89d68a36b1eafe8e0175f
Giovanna <grspmarino@santoandre.sp.gov.br>
2023-07-17 15:22:56 -0300

    ponteiros

---
+++ Curso C e C++/Ponteiros/Ponteiros.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <stdbool.h>
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+ 
+    //Para usar acentos
+    setlocale(LC_ALL,"");
+
+    //Declarando variaveis
+    int a = 20;
+
+    //imprimindo o valor de uma variavel 
+    printf("O valor de a : %d\n", a);
+
+    //imprimindo o endereço de uma variavel 
+    printf("O endereço de a : %d\n", &a);
+
+    //Lendo o valor de uma variavel
+    cout << "Digite um novo valor para a : ";
+    scanf("%d",&a);
+
+    //Imprimindo o valor de uma variavel
+    cout << "O valor atual de a : " << a <<" ";
+
+    return 0;
+}
